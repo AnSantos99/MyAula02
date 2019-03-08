dd65e948d61c646f29e1f7391b8379f4969305ab
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 10:17:36 +0000

    Ex2 Varios types of strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -11,9 +11,22 @@ namespace VariasStrings
             string welcome = "Hello World!\n";
             string line1 = "How\tare\tyou\n";
             string verbatim = @"It's a great day\u1416\n";
-            string unicodechars = "This is a good sign\u1416\n";
+            string unicodechars = "This is a good sign\u1416";
             string normalVerbatim = @"""Hello how are you\n""";
-            Console.WriteLine("{0}{1}{2}{3}{4}", welcome, line1, verbatim, unicodechars, normalVerbatim);
+            Console.WriteLine("{0}{1}{2}{3}{4}", welcome, line1, verbatim,
+            unicodechars, normalVerbatim);
+
+            //Ex2 Diferent outputs of strings
+            int v1 = 1;
+            int v2 = 3;
+            string personInfo = String.Format("Name is {0} age ={1}", "Ana", 23);
+            string placement = String.Format("First is {1} and {0}", v1, v2);
+            string adds = $"{v1} mais {v2} = {v1 + v2}";
+            Console.WriteLine("\nEx2 Strings");
+            Console.WriteLine("{1}{0}", personInfo, placement);
+            Console.WriteLine(adds);
+            Console.Read();
+
         }
     }
 }
