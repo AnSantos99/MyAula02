a84c4ef3729238faa749160fed563e47a8d3a70b
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 09:43:31 +0000

    Ex1 VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-
+using System.Text;
 namespace VariasStrings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string welcome = "Hello World!"
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string welcome = "Hello World!\n";
+            string line1 = "How\tare\tyou\n";
+            string verbatim = @"It's a great day\u1416\n";
+            string unicodechars = "This is a good sign\u1416\n";
+            string normalVerbatim = @"""Hello how are you\n""";
+            Console.WriteLine("{0}{1}{2}{3}{4}", welcome, line1, verbatim, unicodechars, normalVerbatim);
         }
     }
 }
