2f5a0b96dca9ab659b6029ea3aed08cc206c7b82
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 12:29:59 +0000

    Primeiro programa Cilindro

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,7 +9,8 @@ namespace Cilindro
             // Declarar variaveis
             string alturaS, raioS;
             float alturaF, raioF;
-
+            float volume, area;
+            float pi = 3.1415926f;
 
             // Pedir altura
             Console.WriteLine("Introduz altura: ");
@@ -20,12 +21,22 @@ namespace Cilindro
 
             // Converter altura em float
             alturaF = Convert.ToSingle(alturaS);
-            raioF = Convert.ToSingle(raioS);
+
             // Pedir o raio e converter o raio em float
+            raioF = Convert.ToSingle(raioS);
+
             // Calcular o volume
+            volume = pi * raioF * raioF * alturaF;
+
             // Calcular area superficial
+            area = 2 * pi * raioF * (raioF + alturaF);
+
             // Mostrar resultados
-       
+            Console.WriteLine($"Volume é {volume:f3}");
+            //Console.WriteLine($"A area é {area:f3}");
+            Console.WriteLine("A area é {0:f3}", area);
+
+            Console.Read();
 
         }
     }
