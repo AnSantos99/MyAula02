218854f91f12d0b111257ebebe5c14149464b09a
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 09:21:35 +0000

    Add project files.

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcome = "Hello World!"
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
