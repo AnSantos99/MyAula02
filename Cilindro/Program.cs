77b926f8515239cb3a2d16f1a7ffbce322dd166f
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 12:12:16 +0000

    Converção de strings em Floats

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,13 +7,20 @@ namespace Cilindro
         static void Main(string[] args)
         {
             // Declarar variaveis
-            string alturaS;
+            string alturaS, raioS;
+            float alturaF, raioF;
+
+
             // Pedir altura
             Console.WriteLine("Introduz altura: ");
             alturaS = Console.ReadLine();
 
+            Console.WriteLine("Introduz o raio");
+            raioS = Console.ReadLine();
+
             // Converter altura em float
-            
+            alturaF = Convert.ToSingle(alturaS);
+            raioF = Convert.ToSingle(raioS);
             // Pedir o raio e converter o raio em float
             // Calcular o volume
             // Calcular area superficial
