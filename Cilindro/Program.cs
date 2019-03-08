635bcf4c8ad7e3b1d10a9a9287a22533c7a52c76
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-08 12:05:55 +0000

    Pedir altura ao utilizador

---
+++ Cilindro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarar variaveis
+            string alturaS;
+            // Pedir altura
+            Console.WriteLine("Introduz altura: ");
+            alturaS = Console.ReadLine();
+
+            // Converter altura em float
+            
+            // Pedir o raio e converter o raio em float
+            // Calcular o volume
+            // Calcular area superficial
+            // Mostrar resultados
+       
+
+        }
+    }
+}
